a36e22a89070cebdbccfa38dac1770194a402f3d
Blankeos <carloantonioct@gmail.com>
2023-03-18 10:10:35 +0800

    add: vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import vercelStatic from "@astrojs/vercel/static";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
+  output: "server",
+  adapter: vercelStatic(),
 });
