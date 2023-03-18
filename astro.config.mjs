6d994d7f31da715a87bd8abda7b1eb1f8a4a517e
Blankeos <carloantonioct@gmail.com>
2023-03-18 10:11:26 +0800

    fix: serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-import vercelStatic from "@astrojs/vercel/static";
+import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "server",
-  adapter: vercelStatic(),
+  adapter: vercelServerless(),
 });
