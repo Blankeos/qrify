3871c58711e16a433a1d5e002f4cad77e08b018d
Blankeos <carloantonioct@gmail.com>
2023-03-18 22:58:55 +0800

    feat: changed @hello-pangea/color-picker to react-colorful

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,4 +1,5 @@
-import { ChromePicker } from "@hello-pangea/color-picker";
+// import { ChromePicker } from "@hello-pangea/color-picker";
+import { HexColorPicker } from "react-colorful";
 import Tippy from "@tippyjs/react";
 
 interface IColorPickerProps {
@@ -13,29 +14,28 @@ export const ColorPicker: React.FC<IColorPickerProps> = ({
 }) => {
   return (
     <div className="relative h-16 w-16">
-      <Tippy content={hoverContent} hideOnClick={true}>
-        <div>
-          <Tippy
-            theme="transparent"
-            interactive={true}
-            trigger="click"
-            arrow={false}
-            content={
-              <div className="">
-                <ChromePicker
+      <Tippy content={hoverContent}>
+        <Tippy
+          theme="transparent"
+          interactive={true}
+          trigger="click"
+          arrow={false}
+          content={
+            <div className="">
+              <HexColorPicker color={color} onChange={setColor} />
+              {/* <ChromePicker
                   disableAlpha={true}
                   color={color}
                   onChange={(v) => setColor(v.hex)}
-                />
-              </div>
-            }
-          >
-            <div
-              className="absolute h-16 w-16 rounded-md border cursor-pointer"
-              style={{ backgroundColor: color }}
-            ></div>
-          </Tippy>
-        </div>
+                /> */}
+            </div>
+          }
+        >
+          <div
+            className="absolute h-16 w-16 rounded-md border cursor-pointer"
+            style={{ backgroundColor: color }}
+          ></div>
+        </Tippy>
       </Tippy>
     </div>
   );
